a7ba3586eeae0ccc1f72844e3dba6148906db15b
Mftech-Git <153482314+Mftech-Git@users.noreply.github.com>
2024-03-13 14:17:38 -0700

    Update RetroTv.js

--- src/components/AboutUs/RetroTv.js
+++ src/components/AboutUs/RetroTv.js
@@ -1,55 +1,68 @@
-import React, { useState } from 'react';
-import tvImage from '../../images/RetroTv.png'; // Ensure this is the correct path
-import fairImage from '../../images/about/fair.png'; // The image to show when video ends
+import React, { useState, useRef } from 'react';
+import tvImage from '../../images/RetroTv.png';
+import fairImage from '../../images/about/fair.png';
 
 const RetroTv = () => {
   const [videoEnded, setVideoEnded] = useState(false);
+  const videoRef = useRef(null);
 
-  // This function will be triggered when the iframe's content loads
   const handleLoad = (event) => {
-    // Accessing the contentWindow property of the iframe
-    const iframe = event.target;
+    // The rest of your handleLoad function...
+  };
+
+  const handlePlayVideo = () => {
+    const iframe = videoRef.current;
     const player = iframe.contentWindow;
+    player.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
+  };
 
-    // Listen for messages from the iframe
-    window.addEventListener('message', (event) => {
-      try {
-        const message = JSON.parse(event.data);
+  // Disables context menu
+  const handleContextMenu = (event) => {
+    event.preventDefault();
+  };
 
-        // Check if the video has ended
-        if (message.event === 'onStateChange' && message.info === 0) {
-          setVideoEnded(true);
-        }
-      } catch (error) {
-        // In case the message data isn't JSON or another error occurs
-        console.error('Message from iframe could not be parsed:', error);
-      }
-    });
-
-    // Post a message to the iframe to add event listeners
-    player.postMessage(
-      '{"event":"listening","id":null,"channel":"widget","func":"addEventListener","args":["onStateChange"]}',
-      '*'
-    );
+  // Disables keyboard shortcuts
+  const disableKeyboardControls = (event) => {
+    // Key codes for spacebar and arrow keys, add more if needed
+    const forbiddenKeys = [' ', 'ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown'];
+    if (forbiddenKeys.includes(event.key)) {
+      event.preventDefault();
+    }
   };
 
   return (
-    <div className="tv-container">
+    <div className="tv-container" style={{ position: 'relative' }}>
       {!videoEnded ? (
-        // If the video has not ended, display the YouTube player
-        <iframe
-          className="video-element"
-          width="100%"
-          height="100%"
-          src="https://www.youtube.com/embed/IQHs-IEyCkQ?enablejsapi=1&version=3&playerapiid=ytplayer&rel=0"
-          title="YouTube video player"
-          frameBorder="0"
-          onLoad={handleLoad} // Call handleLoad when iframe has loaded
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
+        <>
+          <iframe
+            ref={videoRef}
+            className="video-element"
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/QMEhws2qCPE?controls=1&rel=0&autoplay=0&modestbranding=1&disablekb=1"
+            title="YouTube video player"
+            frameBorder="0"
+            onLoad={handleLoad}
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            style={{ zIndex: 1 }}
+            onContextMenu={handleContextMenu}
+          ></iframe>
+          <div
+            className="overlay"
+            onClick={handlePlayVideo}
+            onContextMenu={handleContextMenu}
+            style={{
+              position: 'absolute',
+              top: 0,
+              bottom: 0,
+              right: 0,
+              left: 0,
+              zIndex: 2, // Higher z-index so it's above the iframe
+              pointerEvents: 'none', // This will make the overlay not respond to mouse events except onClick
+            }}
+          ></div>
+        </>
       ) : (
-        // Otherwise, display the 'fair' image
         <img src={fairImage} alt="Ending Display" className="ending-image" />
       )}
       <img src={tvImage} alt="Retro TV" className="tv-frame" />
